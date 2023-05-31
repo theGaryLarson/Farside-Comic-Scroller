2e5ef280df99ab6accc2130f20098a4652880d6a
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 12:23:59 -0700

    Increased surface are of image to make more readable.

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -18,12 +18,33 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import com.example.affirmations.model.Comic
 
 class MainActivity : ComponentActivity() {
 
@@ -34,9 +55,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Gray
                 ) {
-                    AffirmationsApp()
+                    FarsideComicsApp()
                 }
             }
         }
@@ -44,5 +65,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AffirmationsApp() {
+fun FarsideComicsApp() {
+    FarsideComicList(
+        comicList = Datasource().loadCaptions(),
+    )
+}
+
+@Composable
+fun ComicCard(comic: Comic, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        colors = cardColors(
+            contentColorFor(backgroundColor = Color.DarkGray)
+        )
+    ) {
+        Column(
+            modifier = modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id=comic.imageResourceId),
+                contentDescription = stringResource(id=comic.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = LocalContext.current.getString(comic.stringResourceId),
+                modifier = Modifier
+                    .padding(16.dp),
+                color = Color.White,
+                style = MaterialTheme.typography.headlineSmall,
+
+            )
+        }
+    }
+}
+
+@Composable
+fun FarsideComicList(comicList: List<Comic>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(comicList) { caption ->
+            ComicCard(
+                comic = caption,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ComicCardPreview() {
+    ComicCard(Comic(R.string.caption1, R.drawable.img1))
 }
--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -15,24 +15,27 @@
  */
 package com.example.affirmations.data
 
-//import com.example.affirmations.R
-//import com.example.affirmations.model.Affirmation
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Comic
 
 /**
- * [Datasource] generates a list of [Affirmation]
+ * [Datasource] generates a list of [Comic]
  */
 class Datasource() {
-//    fun loadAffirmations(): List<Affirmation> {
-//        return listOf<Affirmation>(
-//            Affirmation(R.string.affirmation1, R.drawable.image1),
-//            Affirmation(R.string.affirmation2, R.drawable.image2),
-//            Affirmation(R.string.affirmation3, R.drawable.image3),
-//            Affirmation(R.string.affirmation4, R.drawable.image4),
-//            Affirmation(R.string.affirmation5, R.drawable.image5),
-//            Affirmation(R.string.affirmation6, R.drawable.image6),
-//            Affirmation(R.string.affirmation7, R.drawable.image7),
-//            Affirmation(R.string.affirmation8, R.drawable.image8),
-//            Affirmation(R.string.affirmation9, R.drawable.image9),
-//            Affirmation(R.string.affirmation10, R.drawable.image10))
-//    }
+    fun loadCaptions(): List<Comic> {
+        return listOf<Comic>(
+            Comic(R.string.caption1, R.drawable.img1),
+            Comic(R.string.caption2, R.drawable.img2),
+            Comic(R.string.caption3, R.drawable.img3),
+            Comic(R.string.caption4, R.drawable.img4),
+            Comic(R.string.caption5, R.drawable.img5),
+            Comic(R.string.caption6, R.drawable.img6),
+            Comic(R.string.caption7, R.drawable.img7),
+            Comic(R.string.caption8, R.drawable.img8),
+            Comic(R.string.caption9, R.drawable.img9),
+            Comic(R.string.caption10, R.drawable.img10),
+            Comic(R.string.caption11, R.drawable.img11),
+            Comic(R.string.caption12, R.drawable.img12))
+    }
 }
